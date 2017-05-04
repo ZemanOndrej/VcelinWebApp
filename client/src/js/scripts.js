a591458f68465f76d4fb0dbf3c086131a2325033
Ondrej Zeman <ozeman77@gmail.com>
2017-05-04 21:35:32 +0200

    complete structure with react

--- client/src/js/scripts.js
+++ client/src/js/scripts.js
@@ -1,3 +1,10 @@
 /**
  * Created by ozema on 04-May-17.
  */
+import Layout from "./components/module1"
+import  ReactDOM from "react-dom";
+import  React from "react";
+
+
+const app  = document.querySelector("#app");
+ReactDOM.render(<Layout/>,app);
